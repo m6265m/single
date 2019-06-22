5b5f9b7e6dea0ecff48d4fec65ee28e780c7499d
Maha Tausif <tausif.maha@gmail.com>
2019-06-22 08:32:01 -0700

    Initial commit

---
+++ single.js
@@ -0,0 +1,63 @@
+$(window).on('scroll', function () {
+    if($(window).scrollTop()){
+        $('#navbar').addClass('black')
+        $('#navbar').addClass('drop-shadow')
+        $('.sign-in').addClass('scrolling')
+    }
+    else{
+        $('#navbar').removeClass('black')
+        $('#navbar').removeClass('drop-shadow')
+        $('.sign-in').removeClass('scrolling')
+    }
+})
+
+$(function() {
+    $('.fa-user').hover(function() {
+        $('.sign-in').css('visibility', 'visible');
+    }, function() {
+        // on mouseout, reset the background colour
+        $('.sign-in').css('visibility', 'hidden');
+    });
+});
+$(function() {
+    $('.sign-in').hover(function() {
+        $('.sign-in').css('visibility', 'visible');
+    }, function() {
+        // on mouseout, reset the background colour
+        $('.sign-in').css('visibility', 'hidden');
+    });
+});
+
+var x = 0;
+
+
+const minusbutton = document.getElementById("minus");
+const plusbutton = document.getElementById("plus");
+let v = document.getElementById("form-input1").innerHTML("value");
+
+plusbutton.addEventListener("click", () => {
+    v++;
+    document.getElementById("form-input1").innerHTML= v;
+});
+
+
+
+
+
+
+const hamburger = document.getElementById("menuButton");
+const navLinks = document.getElementById("menu");
+const links = document.getElementById("menu li");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+});
+
+
+
+
+
